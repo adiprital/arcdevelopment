cafe63a47d8c3fc6df177531b1cca4971cf0aa88
Adi Pri Tal <adiprital@gmail.com>
2022-07-30 20:04:45 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './ui/Header';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './ui/Theme';
+// import ThemeProvider from '@material-ui/core/styles';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      Hello!
+    </ThemeProvider>
+  );
+}
+
+export default App;
